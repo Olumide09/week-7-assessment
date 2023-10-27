3aa2ff49704a6f7d2f0902dd5dd8c8f0e4013e34
Olumide Olaoye <92790819+Olumide09@users.noreply.github.com>
2023-10-27 11:38:06 -0500

    finished sumZero.js

---
+++ sumZero.js
@@ -0,0 +1,16 @@
+function sumZer0(arr) {
+  for (let i = 0; i < arr.length; i = i + 1) {
+    for (let j = 0; j < arr.length; j = j + 1) {
+      if (arr[i] + arr[j] === 0) {
+        return true;
+      } 
+    }
+  }
+    return false;
+  
+}
+
+console.log(sumZer0([1, 2, 3, -2]));
+
+
+// code runtime = O(n^2) it has 2 for loops
